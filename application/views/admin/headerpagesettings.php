b200c7a85d70ca5e81ea720f07bdbf830453e8bc
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2021-03-21 08:43:14 +0530

    dont know

---
+++ application/views/admin/headerpagesettings.php
@@ -0,0 +1,142 @@
+
+	        	<div class="container-fluid">
+	        	    <div class="row mt-1 py-1" style="background-color: transparent;">
+
+	        	    	<div class="col-md-12 p-0">
+		        	    	<nav aria-label="breadcrumb">
+		        	    	  <ol class="breadcrumb">
+		        	    	    <li class="breadcrumb-item"><a href="dashboard.html">Dashboard</a></li>
+		        	    	    <li class="breadcrumb-item active" aria-current="page">Home Page</li>
+		        	    	  </ol>
+		        	    	</nav>
+	        	    	</div>
+
+	        	    	<div class="col-md-4 p-3" style="background-color:rgba(255, 255, 255, 0.5)">
+	        	    	
+	        	    			<form action="">
+	        	    				<div class="form-group">
+	        	    					<label for="section-title-1">
+	        	    						Section Title
+	        	    					</label>
+	        	    					<input class="form-control" type="text" id="section-title-1" name="section-title-1">
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<label for="section-image">Section Image</label>
+	        	    					<div class="custom-file">
+	        	    					    <input type="file" class="custom-file-input" id="customFile">
+	        	    					    <label class="custom-file-label" for="customFile">Choose file</label>
+	        	    					  </div>
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<label for="sectionDescription">Section Description</label>
+	        	    					<textarea id="summernote" name="sectionDescription" rows="10"></textarea>
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<input type="submit" value="submit" class="btn btn-warning form-control">
+	        	    				</div>
+	        	    			</form>
+	        	    	</div>
+	        	    	<div class="col-md-4 p-3" style="background-color:rgba(255, 255, 255, 0.5)">
+	        	    	
+	        	    			<form action="">
+	        	    				<div class="form-group">
+	        	    					<label for="section-title-1">
+	        	    						Section Title
+	        	    					</label>
+	        	    					<input class="form-control" type="text" id="section-title-1" name="section-title-1">
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<label for="section-image">Section Image</label>
+	        	    					<div class="custom-file">
+	        	    					    <input type="file" class="custom-file-input" id="customFile">
+	        	    					    <label class="custom-file-label" for="customFile">Choose file</label>
+	        	    					  </div>
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<label for="sectionDescription">Section Description</label>
+	        	    					<textarea id="summernote1" name="sectionDescription" rows="10"></textarea>
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<input type="submit" value="submit" class="btn btn-warning form-control">
+	        	    				</div>
+	        	    			</form>
+	        	    	</div>
+	        	    	<div class="col-md-4 p-3" style="background-color:rgba(255, 255, 255, 0.5)">
+	        	    	
+	        	    			<form action="">
+	        	    				<div class="form-group">
+	        	    					<label for="section-title-1">
+	        	    						Section Title
+	        	    					</label>
+	        	    					<input class="form-control" type="text" id="section-title-1" name="section-title-1">
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<label for="section-image">Section Image</label>
+	        	    					<div class="custom-file">
+	        	    					    <input type="file" class="custom-file-input" id="customFile">
+	        	    					    <label class="custom-file-label" for="customFile">Choose file</label>
+	        	    					  </div>
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<label for="sectionDescription">Section Description</label>
+	        	    					<textarea id="summernote2" name="sectionDescription" rows="10"></textarea>
+	        	    				</div>
+	        	    				<div class="form-group">
+	        	    					<input type="submit" value="submit" class="btn btn-warning form-control">
+	        	    				</div>
+	        	    			</form>
+	        	    	</div>
+	        	    
+	        	    </div>
+	        	</div>    
+	        	<script type="text/javascript">
+	        	    $(document).ready(function () {
+	        	        $('#sidebarCollapse').on('click', function () {
+	        	            $('#sidebar').toggleClass('active');
+	        	            $(this).toggleClass('active');
+	        	        });
+	        	    });
+	        	</script>
+
+	        	<script>
+	        	document.addEventListener( 'DOMContentLoaded',function(){
+	        	 var searchpara=document.getElementById("searchContent").innerHTML;
+	        	 searchpara=searchpara.toString();
+	        	 document.getElementById("searchBtn").onclick =function ()
+	        	 {highlight_word(searchpara)};	
+	        	},false);
+
+	        	function highlight_word(searchpara)
+	        	{
+	        	 var text=document.getElementById("searchIp").value;
+	        	 if(text)
+	        	 {
+	        	  var pattern=new RegExp("("+text+")", "gi");
+	        	  var new_text=searchpara.replace(pattern, "<span class='highlight'>"+text+"</span>");
+	        	  document.getElementById("searchContent").innerHTML=new_text;
+	        	 }
+	        	}
+
+	        	</script>
+	        	<script>
+	        	$(document).ready(function() {
+	        	   var t = $('#summernote').summernote(
+	        	   	{
+	        	   		height: 150,
+	        	   		dialogsInBody: true
+	        	 	});
+	        	   var t1 = $('#summernote1').summernote(
+	        	   	{
+	        	   		height: 150,
+	        	   		dialogsInBody: true
+	        	 	});
+	        	   var t2 = $('#summernote2').summernote(
+	        	   	{
+	        	   		height: 150,
+	        	   		dialogsInBody: true
+	        	 	});
+        	  		$("#btn").click(function(){
+        	     		$('div.note-editable').height(150);
+					});
+	        	});
+	        	</script>	
---
+++ application/views/dbtables.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<form id="newTableForm" action="<?php echo base_url('create-new-table'); ?>" method="POST">
+	<div class="form-row border-bottom border-primary rounded-left p-1 mb-1 d-flex justify-content-between">
+	  <label>Table Name: <input type="text" name="table_name" placeholder="Table Name"></label>
+	  <input type="button" class="btn btn-sm btn-primary" id="addColumnBtn" value="Add Another Field">
+      
+	</div> 
+	  	
+	  <div id="row-container">
+	  	  
+	  <div class="field-row form-row border border-secondary rounded p-1 mt-1">
+	    <div class="form-group col-md-3">
+	      <label>Field Name</label>
+	      <input type="text" class="form-control" name="field1[]" placeholder="Field Name">
+	    </div>
+	    <div class="form-group col-md-3">
+	      <label for="inputState">Data Type</label>
+	      <!-- <select id="inputState" class="form-control" name="field1[]">
+	        <option selected>Choose...</option>
+	        <option>...</option>
+	      </select> -->
+	      <input type="text" class="form-control" name="field1[]" placeholder="Data Type">
+	    </div>
+	    <div class="form-group col-md-3">
+	      <label>Length</label>
+	      <input type="text" class="form-control" placeholder="Length" name="field1[]">
+	    </div>
+	    <div class="form-group col-md-2">
+	        <div class="col-md-12 d-flex justify-content-between">
+	      		<label>Auto Increment</label>
+	          	<input type="checkbox" id="gridCheck" name="field1[]" checked>
+	        </div>
+	        <div class="col-md-12 d-flex justify-content-between">
+	      	  	<label>Primary Key</label>
+	          	<input type="checkbox" id="gridCheck" name="field1[]" checked>
+	        </div>
+	    </div>
+	    	    
+	  </div>
+  </div>
+  	<div class="form-group mt-2">
+  		<button type="submit" class="btn btn-primary">Create New Table</button>
+  		<button type="button" class="btn btn-danger">Cancel</button>
+	</div>
+</form>
+
+<script type="text/javascript">
+	$("#addColumnBtn").click(function(){
+		var presentFieldCount = $(".field-row").length;
+		$("#row-container").append('<div class="field-row form-row border border-secondary rounded p-1 mt-1"><div class="form-group col-md-3"><label>Field Name</label><input type="text" class="form-control" name="field'+presentFieldCount+'[]" placeholder="Field Name"></div><div class="form-group col-md-2"><label for="inputState">Data Type</label><input type="text" class="form-control" name="field'+presentFieldCount+'[]" placeholder="Data Type"></div><div class="form-group col-md-1"><label>Length</label><input type="text" class="form-control" placeholder="Length" name="field'+presentFieldCount+'[]"></div><div class="form-group col-md-3"><label>Default Value</label><input type="text" class="form-control" placeholder="Custom or NULL" name="field'+presentFieldCount+'[]"></div> <div class="form-group col-md-1"><label>Unique</label><input type="checkbox" class="form-control" id="gridCheck" name="field'+presentFieldCount+'[]"></div><div class="form-group col-md-2"><label>Remove Field</label><button type="button" class="btn-danger fa fa-minus form-control" onclick="remRow('+presentFieldCount+')"></button></div></div>');
+	});
+
+	function remRow(fieldRowId) {
+		
+		var fieldRowsLength = $(".field-row");
+		$(fieldRowsLength[fieldRowId]).remove();
+		// alert(fieldRowId);
+	}
+</script>
